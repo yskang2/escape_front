dd2f4ac48878c03ca92557cc53a1cb47791afaf3
hyein27 <pgigpdlls@naver.com>
2023-08-20 17:54:03 +0900

    페이지 추가, 이미지 경로 변경, 햄버거 버튼 퍼블리싱 수정, 파비콘 변경, text editor 추가

---
+++ static/js/commom.js
@@ -0,0 +1,20 @@
+// 택스트 editor
+$(document).ready(function() {
+    $('#summernote').summernote();
+  });
+
+
+// 썸네일 등록 js
+function setThumbnail(event) {
+    var reader = new FileReader();
+
+    reader.onload = function(event) {
+      var img = document.createElement("img");
+      img.setAttribute("src", event.target.result);
+      document.querySelector("div#image_container").appendChild(img);
+      document.querySelector("div#image_container").style.width='300px';
+      document.querySelector("div#image_container").style.overflow = 'hidden';
+    };
+
+    reader.readAsDataURL(event.target.files[0]);
+}
