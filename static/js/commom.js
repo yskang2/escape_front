13e35eb700f35154eace856a68331e2788d3002f
hyein27 <pgigpdlls@naver.com>
2023-08-25 15:23:12 +0900

    post.html 추가

--- static/js/commom.js
+++ static/js/commom.js
@@ -25,3 +25,61 @@ $(function() {
   $("footer").load("/footer.html")
 })
 
+
+// 닉네임 변경
+document.addEventListener('DOMContentLoaded', function () {
+  const userList = document.getElementById('user-list');
+  const changeNicknameForm = document.getElementById('change-nickname-form');
+  const changeNicknameButton = document.getElementById('change-nickname-button');
+
+  // 초기 사용자 목록을 표시합니다.
+  updateUsers();
+
+  changeNicknameButton.addEventListener('click', function () {
+      const username = document.getElementById('username').value;
+      const newNickname = document.getElementById('new_nickname').value;
+
+      if (username && newNickname) {
+          changeNickname(username, newNickname);
+      }
+  });
+
+  async function updateUsers() {
+      try {
+          const response = await fetch('/get_users'); // 서버에서 사용자 목록을 가져옵니다.
+          const users = await response.json();
+
+          userList.innerHTML = ''; // 기존 목록 초기화
+
+          users.forEach(function (user) {
+              const listItem = document.createElement('li');
+              listItem.textContent = `${user.username}: ${user.nickname}`;
+              userList.appendChild(listItem);
+          });
+      } catch (error) {
+          console.error('Error updating users:', error);
+      }
+  }
+
+  async function changeNickname(username, newNickname) {
+      try {
+          const response = await fetch('/change_nickname', {
+              method: 'POST',
+              headers: {
+                  'Content-Type': 'application/json'
+              },
+              body: JSON.stringify({ username, newNickname })
+          });
+
+          if (response.ok) {
+              updateUsers(); // 사용자 목록을 업데이트합니다.
+          } else {
+              console.error('Nickname change failed');
+          }
+      } catch (error) {
+          console.error('Error changing nickname:', error);
+      }
+  }
+});
+
+
