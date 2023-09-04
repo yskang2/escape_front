3c42dc0da6760241bbda2a7a055b4a7bbb52cea4
yskang2 <90705547+yskang2@users.noreply.github.com>
2023-09-04 14:53:32 +0900

    Update commom.js

--- static/js/commom.js
+++ static/js/commom.js
@@ -185,3 +185,9 @@ function readURL(input) {
       document.getElementById('preview').src = "";
     }
 }
+
+function parseJwt(token) {
+    const base64Url = token.split('.')[1];
+    const base64 = base64Url.replace('-', '+').replace('_', '/');
+    return JSON.parse(atob(base64));
+}
