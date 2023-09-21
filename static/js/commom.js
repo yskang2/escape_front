d691ae380f52727cdcb12ce012fa95751258052f
yskang2 <90705547+yskang2@users.noreply.github.com>
2023-09-21 17:23:39 +0900

    Update commom.js

--- static/js/commom.js
+++ static/js/commom.js
@@ -1,23 +1,23 @@
 
-// $(document).ready(function() {
-//     $('#summernote').summernote();
-//   });
+$(document).ready(function() {
+    $('#summernote').summernote();
+  });
 
 
-// // 썸네일 등록 js
-// function setThumbnail(event) {
-//     var reader = new FileReader();
+// 썸네일 등록 js
+function setThumbnail(event) {
+    var reader = new FileReader();
 
-//     reader.onload = function(event) {
-//       var img = document.createElement("img");
-//       img.setAttribute("src", event.target.result);
-//       document.querySelector("div#image_container").appendChild(img);
-//       document.querySelector("div#image_container").style.width='300px';
-//       document.querySelector("div#image_container").style.overflow = 'hidden';
-//     };
+    reader.onload = function(event) {
+      var img = document.createElement("img");
+      img.setAttribute("src", event.target.result);
+      document.querySelector("div#image_container").appendChild(img);
+      document.querySelector("div#image_container").style.width='300px';
+      document.querySelector("div#image_container").style.overflow = 'hidden';
+    };
 
-//     reader.readAsDataURL(event.target.files[0]);
-// }
+    reader.readAsDataURL(event.target.files[0]);
+}
 
 // //공통헤더
 // $(function() {
