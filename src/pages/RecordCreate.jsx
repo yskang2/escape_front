db333a09528b053913c073fa6484eaff60bea440
yskang2 <90705547+yskang2@users.noreply.github.com>
2024-02-15 01:56:23 +0900

    Update RecordCreate.jsx

--- src/pages/RecordCreate.jsx
+++ src/pages/RecordCreate.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 import { useStateContext } from '../contexts/ContextProvider';
